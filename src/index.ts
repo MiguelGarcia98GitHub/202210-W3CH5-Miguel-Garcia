de6ff39e6ccc21519b9ee94ee2fc57cd1e6ebc79
MiguelGarcia98Github <miguelgarciard98@gmail.com>
2022-10-21 17:43:18 +0200

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,1 @@
-import { Footer } from './components/footer.js';
-import { Header } from './components/header.js';
-import { Main } from './components/main.js';
-import { TaskList } from './components/task.list.js';
-import { TASKS } from './models/data.js';
-
-console.log('Loaded index');
-console.log(TASKS);
-
-new Header('body');
-new Main('body');
-new TaskList('main');
-new Footer('body');
+console.log('Loaded index.js');
